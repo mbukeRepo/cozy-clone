7fb0a166f8cfbda0798da4a60ca43df6bdc315e8
mbukerepo <mbuke404@gmail.com>
2022-11-08 00:12:45 +0200

    fix!(components): renamed my components

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import Burger from "../UI/vectors/burger";
+import Burger from "../UI/vectors/Burger";
 
 export default function Header() {
   return (
